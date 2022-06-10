029ab0ed2e100ce1027abf00535ea39aa0dfd79a
kysongardner <kysongardner1@gmail.com>
2022-06-09 22:59:35 -0500

    added some comments for clarity

--- lib/validator.rb
+++ lib/validator.rb
@@ -10,7 +10,7 @@ class Validator
   def validate
     numbers = @puzzle_string
 
-    # Split numbers into rows of 9
+    # Split numbers into rows 
     line = @puzzle_string.each_line.each_slice(11).to_a
 
     
@@ -19,13 +19,14 @@ class Validator
     
     # Remove any empty strings 
     rows.reject!(&:empty?)
-    
+
+    # Separate out string of 9 numbers into an array with each letter being in it's own index.
     new_rows = []
     rows.each do |row|
       row.each_char {|c| new_rows << c}
     end
     
-    # Bring it back to an array of 9 numbers 
+    # Get rows of 9 numbers
     new_rows = new_rows.each_slice(9).to_a
 
     # See if it's incomplete
@@ -40,16 +41,20 @@ class Validator
       next if @row_valid == false
     end
 
-    # Transpose array so columns become rows so to speak
+    # Transpose 'new_rows' array of arrays so columns become rows so to speak
     columns = new_rows.transpose
 
-    # Check columns for validity
+    # Check columns for validity (I could have just used 'check_row_for_validity' but I made this
+    # 'check_column_for_validity' method for clarity purposes when testing 'column' validity.)
     columns.each do |column|
       @column_valid = check_column_for_validity(column)
       next if @column_valid == false
     end
 
-    # This was a start to testing the subgroups. 
+    # This was a "start" to testing the subgroups. There is an algorithmic way to go about this obviously.
+    # I couldn't think of how to do it but I tried to illustrate my train of thought.
+    # Basically, get a 2D array and grab the nine elements in a subgroup and put them in a list.
+    # I could then use one of my methods e.g. 'check_row_for_validity' to validate the subgroup. 
     top_left_trio = []
     new_rows.each_with_index do |row, i|
       row.each_with_index do |n_row, n_i|
@@ -64,8 +69,6 @@ class Validator
          top_left_trio << n_row if i == 2 && n_i == 2
       end
     end
-
-    raise [@row_valid, @column_valid, @incomplete].inspect
 
     if @row_valid == false || @column_valid == false
       message = 'This sudoku is invalid.'
